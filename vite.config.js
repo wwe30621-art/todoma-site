aa16fe21ec224d1f59f99a88d46793d0337a5e45
sbiba <wwe30621@gmail.com>
2026-01-07 15:19:55 +0800

    build docs

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,8 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+import { fileURLToPath, URL } from "node:url";
+
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
   base: "/todoma-site/",
